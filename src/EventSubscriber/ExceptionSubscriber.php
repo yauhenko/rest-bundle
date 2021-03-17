ab8d5f93ee86e6d4c23cec1b92b0bf5cdccae817
Yauheni Kiryienkau <ya@redstream.by>
2021-03-17 16:18:06 +0300

    v0.1.6

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Error;
@@ -24,27 +25,27 @@ class ExceptionSubscriber implements EventSubscriberInterface {
 
 		$throwable = $event->getThrowable();
 
-		$code = $throwable instanceof Error ? 500 : 400;
+		$code = $throwable instanceof Error ? 500 : $throwable->getCode();
+
+		if($code < 400 || $code > 499) $code = 400;
 
 		if($throwable instanceof BadRequestHttpException) $code = 400;
 		elseif($throwable instanceof AuthenticationException) $code = 401;
 		elseif($throwable instanceof AccessDeniedException) $code = 403;
+		elseif($throwable instanceof AccessDeniedHttpException) $code = 403;
 		elseif($throwable instanceof NotFoundHttpException) $code = 404;
 
 		$message = $throwable->getMessage();
 
-		//if(!$throwable instanceof TranslatedException) {
-			if(preg_match('/^.+\\\([A-z]+) object not found by the @ParamConverter annotation.$/', $message, $m)) {
-				$message = "{$m[1]} not found";
-			} elseif(preg_match('/Access Denied/i', $message)) {
-				$message = 'Access Denied';
-			}
-			//$message = $this->translator->trans($message);
-		//}
+		if(preg_match('/^.+\\\([A-z]+) object not found by the @ParamConverter annotation.$/', $message, $m)) {
+			$message = "{$m[1]} not found";
+		} elseif(preg_match('/Access Denied/i', $message)) {
+			$message = 'Access Denied';
+		}
 
 		$res = [
 			'error' => $message,
-			'code' => $throwable->getCode() ?: $code,
+			'code' => $code,
 		];
 
 		if($this->params->get('env') === 'dev') {
